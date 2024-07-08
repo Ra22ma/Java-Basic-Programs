9dcf10919c8e7b8033dba2e21fb7e63fcf6ba28e
Ra22ma <149290968+Ra22ma@users.noreply.github.com>
2024-07-08 20:13:17 +0530

    Add files via upload

---
+++ CheckNumber.java
@@ -0,0 +1,17 @@
+class CheckNumber 
+{
+	public static void main(String[] args) 
+	{
+		int num=-34;
+		if(num>0){
+			System.out.println("positive number");
+		}
+		else if(num<0){
+			System.out.println("negative number");
+		}
+		else{
+			System.out.println("neutral number");
+		}
+
+	}
+}
---
+++ carDetails.java
@@ -0,0 +1,18 @@
+class carDetails 
+{
+	public static void main(String[] args) 
+	{
+		String carName="KIA";
+		int carSeriesName=14;
+		int carCost=2000000;
+		String carMileage="5 km/l";
+		String carSpeed="120 km/hr";
+		System.out.println("Car name:"+carName);
+		System.out.println("car series:"+carSeriesName);
+		System.out.println("car cost:"+carCost);
+		System.out.println("car mileage:"+carMileage);
+		System.out.println("car Speed:"+carSpeed);
+
+	}
+	
+}
